caf09571693fdd5574810aeb1c6c92c5c587d934
Kirill Kuzmich <kirill.kuzmich.03@mail.ru>
2023-11-04 18:15:30 +0300

    added CRUD posts

--- index.js
+++ index.js
@@ -20,11 +20,13 @@ app.post('/auth/login',loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-// app.get('/posts', PostController.getAll);
-// app.get('/posts:id', PostController.getOne);
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
-// app.delete('/posts', PostController.remove);
-// app.path('/posts', PostController.update);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id',checkAuth, PostController.update);
+
+
 
 
 
